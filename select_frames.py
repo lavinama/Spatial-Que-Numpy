fd43368c735f14c5c2077061cbfbafa1f0747dcb
lavinama <mario.lavina00@gmail.com>
2022-12-14 13:16:19 +0000

    Added code

--- select_frames.py
+++ select_frames.py
@@ -1,9 +1,11 @@
-# Spatial-Que-Numpy
-Write a function to select indices of frames given a clip of the car driving.
+import numpy as np
 
-#### `calculate_distances`
+clip = np.random.random((100, 128, 128, 3))
+frames = np.random.random((100, 128, 128))
+trajectory = np.random.random((10, 2))
 
-```python
+distances_frames = np.random.random((100, 1))
+
 def calculate_distances(trajectory):
     """
     Input: trajectory - np.array of (x, y) points
@@ -13,11 +15,13 @@ def calculate_distances(trajectory):
     distances_points = np.zeros(trajectory.shape[0])
     distances_points[1:] = np.sqrt(np.sum((trajectory[1:] - trajectory[:-1]) ** 2, axis=1))
     return distances_points
-```
 
-#### `match_distance_frames_points`
+def calculate_distances_frames(frames):
+    distances_frames = np.zeros((frames.shape[0], 1))
+    for i in range(frames.shape[0]):
+        distances_frames[i] = np.sum(np.abs(frames[i] - frames[i-1]))
+    return distances_frames
 
-```python
 def match_distance_frames_points(distances_frames, trajectory):
     """
     Input: distances_frames - np.array with the distances between frames
@@ -35,4 +39,5 @@ def match_distance_frames_points(distances_frames, trajectory):
             counter_points += 1
         counter_frames += 1
     return np.array(selected_frames)
-```
+
+print(calculate_distances_frames(frames))
